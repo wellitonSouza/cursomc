67c1ff6dffc563875d3345b733048f5a01ce0c2d
Nelio Alves <wellitonaz@yahoo.com.br>
2019-03-23 16:13:10 -0300

    Criando operacao de instanciacao

--- src/main/java/com/wellitonsouza/m/CursomApplication.java
+++ src/main/java/com/wellitonsouza/m/CursomApplication.java
@@ -1,13 +1,35 @@
 package com.wellitonsouza.m;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wellitonsouza.m.domain.Categoria;
+import com.wellitonsouza.m.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomApplication {
+public class CursomApplication  implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 
 }
